97fc715227837acfe19c7ec06e5ab0684e0d4697
Abhishek <akg002@yahoo.in>
2016-04-02 22:57:55 +0530

    Audio is recieved but noise is played

--- app/src/main/java/in/silive/audiosync/HomeActivity.java
+++ app/src/main/java/in/silive/audiosync/HomeActivity.java
@@ -1,5 +1,8 @@
 package in.silive.audiosync;
 
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
 import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,21 +10,25 @@ import android.util.Log;
 import android.view.View;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.io.PrintWriter;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
-    static final ArrayList<Socket> connectedClient = new ArrayList<>() ;
+    static final ArrayList<DatagramSocket> connectedClient = new ArrayList<>() ;
     final String SERVER_IP = "localhost";
-    final int PORT = 4326;
-    private ServerSocket serverSocket;
+    final int SERVER_PORT = 4326;
+    final int CLIENT_PORT = 4321;
+    private DatagramSocket serverSocket;
     final String TAG = "HomeActivity";
     Thread server ;
     @Override
@@ -46,29 +53,64 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     public class ServerThread implements Runnable{
+        byte[] recieveData = new byte[10240];
+        byte[] sendData = new byte[10240];
 
         @Override
         public void run() {
             try {
-                serverSocket = new ServerSocket(PORT);
+                serverSocket = new DatagramSocket(SERVER_PORT);
                 Log.d(TAG,"Server Listening for clients.");
                 while(true){
-                    Socket client = serverSocket.accept();
-                    connectedClient.add(client);
-                    Log.d(TAG, "Client Connected");
-                    InputStreamReader inputStreamReader = new InputStreamReader(client.getInputStream());
-                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-                    String line;
-                    while ((line = bufferedReader.readLine() ) !=null )
-                    Log.d(TAG, "Message from Client : " + line);
 
-                    PrintStream printStream = new PrintStream(client.getOutputStream());
-                  //  printStream.print("You are connected");
-                    InputStreamReader songInputStream = new InputStreamReader(getApplicationContext().getAssets().open("song.mp3"));
-                    byte[] buffer = new byte[1024];
-                    printStream.flush();
-                    client.shutdownOutput();
+                    DatagramPacket recievePacket = new DatagramPacket(recieveData,recieveData.length);
+                    serverSocket.receive(recievePacket);
+                    Log.d(TAG, "Client Connected");
+                    String str = new String(recievePacket.getData(),0,recievePacket.getLength());
+                    Log.d(TAG, "Client : " + str);
+                    InputStream songStream = getApplicationContext().getAssets().open("song.mp3");
+                    Thread streamer = new Thread(new ClientStreamWriter(recievePacket.getAddress(),
+                            songStream,CLIENT_PORT));
+                    streamer.start();
                 }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+
+    public class ClientStreamWriter implements Runnable{
+        InetAddress clientAddress;
+        int port;
+        InputStream is;
+        byte[] buffer = new byte[10240];
+        DatagramSocket clientSocket;
+
+        public ClientStreamWriter( InetAddress clientAddress, InputStream is, int port) {
+
+            this.clientAddress = clientAddress;
+            this.is = is;
+            this.port = port;
+            try{
+                    clientSocket = new DatagramSocket();
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void run() {
+            try{
+                while ((is.read(buffer,0,buffer.length))> -1 ){
+
+                    DatagramPacket sendPacket = new DatagramPacket(buffer,buffer.length,clientAddress,port);
+                    clientSocket.send(sendPacket);
+                    Thread.sleep(100);
+                }
+
             }catch (Exception e){
                 e.printStackTrace();
             }
@@ -78,22 +120,36 @@ public class HomeActivity extends AppCompatActivity {
 
     public class ClientThread implements Runnable{
         String name;
+        byte[] recieveData = new byte[10240];
+        byte[] sendData = new byte[10240];
         public ClientThread(String name){
-            this.name = name;
+            this.sendData = name.getBytes();
         }
         @Override
         public void run() {
             try {
-                Socket socket = new Socket(SERVER_IP,PORT);
-                Log.d(TAG, "Connected to Server");
-                PrintStream printStream = new PrintStream(socket.getOutputStream());
-                printStream.print(name);
-                socket.shutdownOutput();
 
-                InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
-                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-                Log.d(TAG, "Message from Server : " + bufferedReader.readLine());
+                DatagramPacket sendPacket = new DatagramPacket(sendData,sendData.length,InetAddress.getByName(SERVER_IP),SERVER_PORT);
+                DatagramSocket socket = new DatagramSocket();
+                socket.send(sendPacket);
 
+                DatagramSocket recieverSocket = new DatagramSocket(CLIENT_PORT);
+
+                // Audio Stream initialisieren:
+               int  iAudioBufSize       = AudioTrack.getMinBufferSize(44100, AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT);
+
+                AudioTrack track    = new AudioTrack(AudioManager.STREAM_MUSIC, 44100, AudioFormat.CHANNEL_OUT_STEREO,
+                        AudioFormat.ENCODING_PCM_16BIT, iAudioBufSize, AudioTrack.MODE_STREAM);
+                track.play();
+
+                while (true) {
+                    DatagramPacket recievePacket = new DatagramPacket(recieveData,recieveData.length);
+                    recieverSocket.receive(recievePacket);
+                   // DatagramPacket recievePacket = new DatagramPacket(recieveData, recieveData.length, InetAddress.getByName(SERVER_IP), SERVER_PORT);
+                    String str = new String(recievePacket.getData(), 0, recievePacket.getLength());
+                    Log.d(TAG, "Message from Server : " + recievePacket.getData());
+                    track.write(recievePacket.getData(), 0, recievePacket.getLength());
+                }
 
             }catch (Exception e){
                 e.printStackTrace();
